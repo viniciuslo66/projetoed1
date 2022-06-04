6f4b17d11adcf6086660893b52bfd14d1ee7b068
viniciuslo66 <viniciuslo66@gmail.com>
2022-06-04 20:29:52 -0300

    Teste de erro no heroku

--- src/main/java/com/viniciuslo66/projetoed1/TodolistApplication.java
+++ src/main/java/com/viniciuslo66/projetoed1/TodolistApplication.java
@@ -2,9 +2,10 @@ package com.viniciuslo66.projetoed1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class TodolistApplication {
+public class TodolistApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
